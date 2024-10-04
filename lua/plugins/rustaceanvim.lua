e18c26fb403e3374dc2a3158c66074823cc904c5
Vonr <dtaclanlegion@gmail.com>
2024-10-04 23:06:30 +0800

    update plugins and fix rustaceanvim

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,99 +1,100 @@
 return {
     'mrcjkb/rustaceanvim',
-    version = '^4',
+    version = '^5',
     ft = { 'rust' },
     init = function()
+        ---@type rustaceanvim.Config
+        vim.g.rustaceanvim = {
+            ---@type rustaceanvim.tools.Config
+            tools = {
+                on_initialized = function(_)
+                    -- vim.lsp.inlay_hint.enable()
+                    vim.notify("rust-analyzer started")
+                end,
+            },
+            ---@type rustaceanvim.lsp.ClientConfig
+            server = {
+                cmd = { 'rust-analyzer' },
+                auto_attach = true,
+                standalone = true,
+                settings = {
+                    ['rust-analyzer'] = {
+                        checkOnSave = true,
+                        check = {
+                            enable = true,
+                            features = 'all',
+                            allTargets = true,
+                            overrideCommand = {
+                                'cargo', 'clippy',
+                                '--workspace',
+                                '--message-format=json',
+                                '--all-targets',
+                                '--',
+                                '-D', 'clippy::correctness',
+                                '-D', 'clippy::complexity',
+                                '-D', 'clippy::perf',
+                                '-D', 'clippy::style',
+                            },
+                        },
+                        cargo = {
+                            buildScripts = {
+                                enable = true
+                            },
+                            features = 'all',
+                        },
+                        procMacro = {
+                            enable = true,
+                        },
+                        completion = {
+                            snippets = {
+                                custom = {
+                                    ['Collect into Vec'] = {
+                                        postfix = 'cv',
+                                        body = '${receiver}.collect::<Vec<_>>()',
+                                        description = 'Collects iterator into a Vec of an inferred type.',
+                                        scope = 'expr',
+                                    },
+                                    ['Collect'] = {
+                                        postfix = 'cl',
+                                        body = '${receiver}.collect::<$1>()',
+                                        description = 'Collects iterator into a Vec of an inferred type.',
+                                        scope = 'expr',
+                                    },
+                                    ['Filter Map Results'] = {
+                                        postfix = 'oks',
+                                        body = '${receiver}.filter_map(Result::ok)',
+                                        description =
+                                        'Filters an iterator of Results to Ok variants and maps it to its inner type.',
+                                        scope = 'expr',
+                                    },
+                                    ['Wrap with new'] = {
+                                        postfix = 'new',
+                                        body = '$1::new(${receiver})$0',
+                                        scope = 'expr',
+                                    },
+                                    ['Drop'] = {
+                                        postfix = 'drop',
+                                        body = 'drop(${receiver})',
+                                        scope = 'expr',
+                                    },
+                                    ['Clone Self'] = {
+                                        postfix = 'clsf',
+                                        body = 'let ${receiver} = ${receiver}.clone();$1',
+                                        scope = 'expr',
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        }
+
+        vim.g.rustfmt_autosave = 1
+
         vim.api.nvim_create_autocmd('BufReadPre', {
             pattern = '*.rs',
             callback = function()
-                if not vim.g.rustaceanvim then
-                    ---@type RustaceanOpts
-                    vim.g.rustaceanvim = {
-                        ---@type RustaceanToolsOpts
-                        tools = {
-                            on_initialized = function(_)
-                                -- vim.lsp.inlay_hint.enable()
-                                vim.notify("rust-analyzer started")
-                            end,
-                            enable_clippy = true,
-                        },
-                        ---@type RustaceanLspClientOpts
-                        server = {
-                            cmd = { 'rust-analyzer' },
-                            auto_attach = true,
-                            standalone = true,
-                            settings = {
-                                checkOnSave = {
-                                    allFeatures = true,
-                                    overrideCommand = {
-                                        'cargo', 'clippy',
-                                        '--workspace',
-                                        '--message-format=json',
-                                        '--all-targets',
-                                        '--',
-                                        '-D', 'clippy::correctness',
-                                        '-D', 'clippy::complexity',
-                                        '-D', 'clippy::perf',
-                                        '-D', 'clippy::style',
-                                    }
-                                },
-                                cargo = {
-                                    buildScripts = true,
-                                    allFeatures = true,
-                                },
-                                completion = {
-                                    snippets = {
-                                        custom = {
-                                            ['Collect into Vec'] = {
-                                                postfix = 'cv',
-                                                body = '${receiver}.collect::<Vec<_>>()',
-                                                description = 'Collects iterator into a Vec of an inferred type.',
-                                                scope = 'expr',
-                                            },
-                                            ['Collect'] = {
-                                                postfix = 'cl',
-                                                body = '${receiver}.collect::<$1>()',
-                                                description = 'Collects iterator into a Vec of an inferred type.',
-                                                scope = 'expr',
-                                            },
-                                            ['Filter Map Results'] = {
-                                                postfix = 'oks',
-                                                body = '${receiver}.filter_map(Result::ok)',
-                                                description =
-                                                'Filters an iterator of Results to Ok variants and maps it to its inner type.',
-                                                scope = 'expr',
-                                            },
-                                            ['Is Not Empty'] = {
-                                                postfix = { 'nmt', 'is_not_empty' },
-                                                body = '!${receiver}.is_empty()',
-                                                scope = 'expr',
-                                            },
-                                            ['Is Empty'] = {
-                                                postfix = { 'mt', 'is_empty' },
-                                                body = '${receiver}.is_empty()',
-                                                scope = 'expr',
-                                            },
-                                            ['Wrap with new'] = {
-                                                postfix = 'new',
-                                                body = '$1::new(${receiver})$0',
-                                                scope = 'expr',
-                                            },
-                                            ['Drop'] = {
-                                                postfix = 'drop',
-                                                body = 'drop(${receiver})',
-                                                scope = 'expr',
-                                            },
-                                        }
-                                    }
-                                }
-                            }
-                        },
-                    }
-
-                    vim.g.rustfmt_autosave = 1
-                end
-
-
                 local buf = vim.api.nvim_get_current_buf()
                 local set = { noremap = true, silent = true, buffer = buf }
 
@@ -114,5 +115,5 @@ return {
                 vim.keymap.set('n', '<Leader>xR', rlsp('runnables'), set)
             end
         })
-    end,
+    end
 }
